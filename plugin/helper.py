86323a0bf3b29ffae46c60817af8e7923a25a6fe
Singularity0909 <904854724@qq.com>
2021-09-22 14:57:16 +0800

    typo: update helper reply

--- plugin/helper.py
+++ plugin/helper.py
@@ -1,13 +1,13 @@
 # coding=utf8
 def get_help(msg):
     return u'现有功能如下：\n\n' + \
-           u'天气预报：@我 {地区}【天气/weather】\n' + \
-           u'一言：@我【一言/hitokoto】\n' + \
-           u'每日一句：@我【每日一句/daily】\n' + \
-           u'笑话：@我【笑话/joke】\n' + \
-           u'知乎日报：@我【知乎/zhihu】\n' + \
-           u'力扣随机一题：@我【随机一题/力扣/leetcode】\n' + \
-           u'智能 (zhang) 闲聊：@我 {内容}\n' + \
-           u'复读机：无需 @我，最近两条消息相同自动触发\n\n' + \
+           u'- 天气预报：@我 {地区}【天气/ weather】\n' + \
+           u'- 一言：@我【一言/ hitokoto】\n' + \
+           u'- 每日一句：@我【每日一句/ daily】\n' + \
+           u'- 笑话：@我【笑话/ joke】\n' + \
+           u'- 知乎日报：@我【知乎/ zhihu】\n' + \
+           u'- 力扣随机一题：@我【随机一题/力扣/ leetcode】\n' + \
+           u'- 智能 (zhàng) 闲聊：@我 {内容}\n' + \
+           u'- 复读机：无需 @我，最近两条消息相同自动触发\n\n' + \
            u'更多功能正在等待主人开发~\n' + \
            u'https://github.com/Singularity0909/wechat-bot'
