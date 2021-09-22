53fa0c19a2202b077e628ad132af7061ab453494
Singularity0909 <904854724@qq.com>
2021-09-22 14:16:49 +0800

    feat: add leetcode

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import itchat
 
 import manager
-from plugin import chat, repeater, helper, weather, hitokoto, daily, joke, zhihu
+from plugin import chat, repeater, helper, weather, hitokoto, daily, joke, zhihu, leetcode
 
 
 @itchat.msg_register('Text', isGroupChat=True)
@@ -21,6 +21,7 @@ manager.set_command([u'一言', 'hitokoto'], True, False, hitokoto.get_hitokoto)
 manager.set_command([u'每日一句', 'daily'], True, False, daily.get_daily)
 manager.set_command([u'笑话', 'joke'], True, False, joke.get_joke)
 manager.set_command([u'知乎', 'zhihu'], True, False, zhihu.get_zhihu)
+manager.set_command([u'力扣', u'随机一题', 'leetcode'], True, False, leetcode.get_random_problem)
 manager.set_command(None, True, True, chat.get_chat_reply)
 manager.set_command(None, False, True, repeater.get_repeat)
 
---
+++ plugin/leetcode.py
@@ -0,0 +1,23 @@
+# coding=utf8
+import random
+
+import requests
+
+PROBLEM_PAGE_URL_FORMAT = 'https://leetcode-cn.com/problems/{}'
+PROBLEM_LIST_API_URL = 'https://leetcode-cn.com/api/problems/algorithms/'
+
+
+def get_random_problem(msg):
+    try:
+        resp = requests.get(PROBLEM_LIST_API_URL)
+        assert resp.status_code == 200
+        data = resp.json()
+        num_total = data['num_total']
+        idx = random.randint(0, num_total - 1)
+        stat = data['stat_status_pairs'][idx]['stat']
+        id = stat['question_id']
+        title = stat['question__title']
+        slug = stat['question__title_slug']
+        return '%d. %s %s' % (id, title, PROBLEM_PAGE_URL_FORMAT.format(slug))
+    except:
+        return u'获取失败'
