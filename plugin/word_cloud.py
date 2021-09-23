6d83f868bf2d0c311133edc3ebbe1a03a4f9fd3c
Singularity0909 <904854724@qq.com>
2021-09-23 13:17:29 +0800

    feat: add new font

--- plugin/word_cloud.py
+++ plugin/word_cloud.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import os
+import random
 
 import itchat
 import jieba
@@ -9,7 +10,10 @@ from plugin import stopword_set
 from util import log
 
 FONT_PATH = 'data'
-FONT_FILE_NAME = 'msyh.ttf'
+FONT_FILE_NAMES = (
+    'msyh.ttf',
+    'pfht.ttf'
+)
 
 IMG_SAVE_PATH = 'temp'
 IMG_FILE_NAME = 'word_cloud.png'
@@ -17,11 +21,10 @@ IMG_FILE_NAME = 'word_cloud.png'
 
 def get_word_cloud(msg):
     try:
-        text = log.get_today_text(msg)
-        words = ' '.join(jieba.cut(text, cut_all=False))
+        words = ' '.join(jieba.cut(log.get_today_text(msg), cut_all=False))
         WordCloud(background_color="white", width=1000, height=800, margin=2,
-                  font_path=os.path.join(FONT_PATH, FONT_FILE_NAME), stopwords=stopword_set).generate(words).to_file(
-            os.path.join(IMG_SAVE_PATH, IMG_FILE_NAME))
+                  font_path=os.path.join(FONT_PATH, random.choice(FONT_FILE_NAMES)), stopwords=stopword_set).generate(
+            words).to_file(os.path.join(IMG_SAVE_PATH, IMG_FILE_NAME))
         itchat.send_image(os.path.join(IMG_SAVE_PATH, IMG_FILE_NAME), msg['FromUserName'])
         return None
     except:
